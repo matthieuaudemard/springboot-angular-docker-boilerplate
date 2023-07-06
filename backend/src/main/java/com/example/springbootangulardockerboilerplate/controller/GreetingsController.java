1b6a19d7e2d91bdf6ed62eed34b925dfe93b633e
matthieuaudemard <matthieu.audemard@gmail.com>
2023-07-06 19:21:19 +0200

    ajout d'un CorsFilter pour permettre la communication entre le front et le back

--- backend/src/main/java/com/example/springbootangulardockerboilerplate/controller/GreetingsController.java
+++ backend/src/main/java/com/example/springbootangulardockerboilerplate/controller/GreetingsController.java
@@ -1,5 +1,8 @@
 package com.example.springbootangulardockerboilerplate.controller;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingsController {
 
+    @Getter
+    @Builder
+    public static class GreetingsDto {
+        private String message;
+    }
+
     @GetMapping
-    public String getApp() {
-        return "Hello World";
+    public ResponseEntity<GreetingsDto> getApp() {
+        return ResponseEntity.ok(GreetingsDto.builder().message("Hello World !").build());
     }
 
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  hellostr: string = "";
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get("http://localhost:8080/api/greetings").subscribe((data: any) => this.hellostr = data.message);
+  }
 }
